4f248f227d5d14636c82a08eb71f5e4ac911b13c
Tymur Tkachenko <tymur.tk@gmail.com>
2022-08-28 18:24:09 -0400

    changed sensor type

--- temp_hum.py
+++ temp_hum.py
@@ -2,5 +2,5 @@ import sys
 import Adafruit_DHT
 
 while True:
-    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
+    humidity, temperature = Adafruit_DHT.read_retry(22, 4)
     print(f'Temp: {temperature} C  Humidity: {humidity} %')
