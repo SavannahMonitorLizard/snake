700e0e05650b06e3f08094b09026801c169dfbf0
Tymur Tkachenko <tymur.tk@gmail.com>
2022-08-28 18:21:34 -0400

    changed pin

--- temp_hum.py
+++ temp_hum.py
@@ -2,5 +2,5 @@ import sys
 import Adafruit_DHT
 
 while True:
-    humidity, temperature = Adafruit_DHT.read_retry(11, 7)
+    humidity, temperature = Adafruit_DHT.read_retry(11, 4)
     print(f'Temp: {temperature} C  Humidity: {humidity} %')
