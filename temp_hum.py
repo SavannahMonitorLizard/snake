a77d7e20876a5fbbd10b02e32e72f914a0f37fb4
Tymur Tkachenko <tymur.tk@gmail.com>
2022-09-10 18:22:59 -0400

    changed sensor pings

--- temp_hum.py
+++ temp_hum.py
@@ -3,7 +3,7 @@ from flask import Flask
 import Adafruit_DHT
 
 while True:
-    humidity1, temperature1 = Adafruit_DHT.read_retry(22, 4)
-    humidity2, temperature2 = Adafruit_DHT.read_retry(22, 17)
+    humidity1, temperature1 = Adafruit_DHT.read_retry(22, 27)
+    humidity2, temperature2 = Adafruit_DHT.read_retry(22, 22)
     print(f'Temp 1: {temperature1} C  Humidity 1: {humidity1} %')
     print(f'Temp 2: {temperature2} C  Humidity 2: {humidity2} %')
